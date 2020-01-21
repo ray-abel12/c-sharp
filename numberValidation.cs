5b1525e9626fdcfadcf222c572cc93ac6c111dd5
ray-abel12 <rayabel12@outlook.com>
2020-01-21 13:05:12 +0100

    commit

---
+++ numberValidation.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace numberValidation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter a number from 1 - 10");
+           
+          var input =Console.ReadLine();
+          var result = Convert.ToInt32(input);
+          if (result >= 1 && result <= 10)
+          {
+              Console.WriteLine("values are within the range of 1-10");
+            }
+          else
+          {
+              Console.WriteLine("out of range/invalid character");
+          }
+
+        }
+    }
+}
