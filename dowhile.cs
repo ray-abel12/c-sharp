ef4e8bf359e77fa34bb243a4f90ad2a2c0375098
ray-abel12 <rayabel12@outlook.com>
2020-01-21 15:34:54 +0100

    commit

---
+++ dowhile.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace doWhileLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("enter name");
+                var input = Console.ReadLine();
+
+                if (string.IsNullOrWhiteSpace(input))
+                {
+                    break;
+                }
+                Console.WriteLine("@echo "+input);
+            }
+        }
+    }
+}
