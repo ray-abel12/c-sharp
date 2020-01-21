52692b9ec63c71b2170d62a065d8091f139b0b30
ray-abel12 <rayabel12@outlook.com>
2020-01-21 14:51:45 +0100

    commit

---
+++ Iteration.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace iteration
+{
+    class Iteration
+    {
+        static void Main(string[] args)
+        {
+           // for (var i = 1; i <= 10; i++)
+          //  {
+           //     if (i % 2 == 0)
+          //      {
+          //          Console.WriteLine(i);
+          //      }
+                
+          //  }
+
+         //   for (var j = 10; j >= 1; j--)
+          //  {
+         //       if (j % 2 == 0)
+           //     {
+           //         Console.WriteLine(j);
+             //   }
+          //  }
+          var name = "abel ray";
+          foreach (var names in name)
+          {
+              Console.Write(names);
+          }
+          var array = new string[4] {"abel ","john ","ray ","tab"};
+          foreach (var i in array)
+          {
+              Console.Write(i);
+          }
+          {
+              
+          }
+          {
+              
+          }
+        }
+    }
+}
