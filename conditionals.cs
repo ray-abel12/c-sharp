a7819e8b2b1440314f357fa4bbd34e667525631e
ray-abel12 <rayabel12@outlook.com>
2020-01-21 11:10:11 +0100

    commit

---
+++ conditionals.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+  public  class Program
+    {
+     public static void Main(string[] args)
+     {
+         Int16 a = 10;
+         Int16 b = 12;
+         Int16 c = 10;
+
+            Console.WriteLine(a < c || b > a);
+            Console.WriteLine((a < c) | (b > a));
+            Console.WriteLine("addition "+ (a+b));
+          
+        }
+    }
+}
