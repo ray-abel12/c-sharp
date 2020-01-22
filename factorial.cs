4921bce472ce76ddcc267de8ec1dac751e9af263
ray-abel12 <rayabel12@outlook.com>
2020-01-22 14:51:57 +0100

    commit

---
+++ factorial.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace factorial
+{
+    class factorial
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter value to get factorial value");
+            var userInput = Console.ReadLine();
+            var convertedInput = Convert.ToInt32(userInput);
+            var total = 1;
+
+            for (int i = 1; i <= convertedInput; i++)
+            {
+              
+                  total*= i;
+                
+            }
+            Console.WriteLine($"the factorial of {convertedInput}! = {total}");
+        }
+    }
+}
