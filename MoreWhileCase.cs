62cb6e0a6eec423ddad95e8e762a624b9b2d5708
ray-abel12 <rayabel12@outlook.com>
2020-01-22 12:45:26 +0100

    commit

---
+++ MoreWhileCase.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace MoreWhileLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            
+            string conditionInput = "";
+            int total = 0;
+            int additionInput = 0;
+            while (conditionInput !="ok")
+            {
+                Console.WriteLine("enter number ");
+                var j = Console.ReadLine();
+                additionInput = Convert.ToInt32(j);
+               
+                    total = total + additionInput; 
+                
+
+                Console.WriteLine("enter 'ok' to  end and sum numbers ");
+                var i = Console.ReadLine();
+                conditionInput = Convert.ToString(i);
+               
+            }
+            Console.WriteLine($"the final value is {total}");
+        }
+    }
+}
