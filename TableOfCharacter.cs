729da9aafcd77028d257abc1ea4327dd4d3451da
ray-abel12 <rayabel12@outlook.com>
2020-01-22 14:28:14 +0100

    commit

---
+++ TableOfCharacter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace table_of_characters
+{
+    class TableOfCharacter
+    {
+        static void Main(string[] args)
+        {
+            char read;
+            for (int i = 1; i <= 122; i++)
+            {
+                read = Convert.ToChar(i);
+                Console.WriteLine($"the ascii value of {i} is {read}");
+            }
+        }
+    }
+}
