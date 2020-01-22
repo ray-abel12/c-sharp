abbd2e3e65b3b98606f06e5c6de58b8410726496
ray-abel12 <rayabel12@outlook.com>
2020-01-22 08:43:11 +0100

    commit

---
+++ PasswordGeneratorProgram.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace RandomNumber
+{
+    public class PasswordGeneratorProgram
+    {
+        static void Main(string[] args)
+        {
+            
+            var random =new Random();
+            var buffer = new char[10];
+            for (int i = 0; i < 10; i++)
+                buffer[i] = ((char) ('a' + random.Next(0, 26)));
+            string password;
+            password = new string(buffer);
+            Console.Write("generated password is ");
+            Console.Write(buffer);
+            
+        }
+    }
+}
---
+++ valueRef.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace valueOrReference
+{
+   public class Person
+   {
+       public int Age;
+        static void Main(string[] args)
+        {
+            var number = 1;
+            Value(number);
+            Console.WriteLine(number);
+            var person = new Person();
+            var age = person.Age = 46;
+            Console.WriteLine(age);
+
+        }
+        public static void Value(int number)
+        {
+            number += 10;
+            
+        }
+
+        public static void MakeOld(Person person)
+       {
+         person.Age += 10;
+        }
+    }
+}
