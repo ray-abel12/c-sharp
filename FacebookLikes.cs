3b4491bbc4f7ef81846a34b7240bbddc97d33b3f
ray-abel12 <rayabel12@outlook.com>
2020-01-23 09:20:21 +0100

    commit

---
+++ FacebookLikes.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FacebookLikes
+{
+    public class FacebookLikes
+
+    {
+        static void Main(string[] args)
+        {
+            
+            var listName = new List<string>();
+          //  var userName = Console.ReadLine();
+          var convertedUserName = "hello";
+            while (convertedUserName != string.Empty)
+            {
+                Console.WriteLine("enter facebook names or enter to end");
+                var userName = Console.ReadLine();
+                convertedUserName = Convert.ToString(userName);
+                listName.Add(convertedUserName);
+
+                
+            }
+
+            if (listName.Count == 0)
+            {
+                Console.WriteLine("no likes");
+            }
+
+            if (listName.Count == 1)
+            {
+                Console.WriteLine($"{listName[0]}  like your post");
+            }
+            if (listName.Count == 2)
+            {
+                Console.WriteLine($"{listName[0]} and {listName[1]}  like your post");
+            }
+            if (listName.Count > 2)
+            {
+                var otherUsers = listName.Count - 2;
+                Console.WriteLine($"{listName[0]} {listName[1]} and {otherUsers} other person likes your post");
+            }
+
+        }
+    }
+}
