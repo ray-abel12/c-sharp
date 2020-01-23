cae2cb21e9c768ec8de6b32a44e75a0ff8a0c08a
ray-abel12 <rayabel12@outlook.com>
2020-01-23 16:53:41 +0100

    commit

---
+++ StringBuilder.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text;
+namespace StringBuilder
+{
+    class StringBuilder
+    {
+        static void Main(string[] args)
+        {
+            var stringBuilder = new System.Text.StringBuilder();
+          stringBuilder.Append('-', 10);
+          stringBuilder.AppendLine();
+          stringBuilder.Append("Header");
+          stringBuilder.AppendLine();
+            stringBuilder.Append('-', 10);
+            stringBuilder.Remove(0, 10);
+            Console.WriteLine(stringBuilder);
+        }
+    }
+}
