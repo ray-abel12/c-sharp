9edf3e4e9cd3d1c720ba9b9c1f37df36d40c6a0a
ray-abel12 <rayabel12@outlook.com>
2020-01-23 15:07:30 +0100

    committ

---
+++ DateTime.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace dateTime
+{
+    public class DateTime
+    {
+        static void Main(string[] args)
+        {
+       var dateTime =new DateTime();
+       var now = System.DateTime.Now;
+       var today = System.DateTime.Today;
+
+     //  Console.WriteLine($"{now.Hour}:{now.Minute}:{now.Second}");
+     var tomorrow = now.AddDays(1);
+     var yerstaday = now.AddDays(-1);
+
+     Console.WriteLine(now.ToLongDateString());
+     Console.WriteLine(now.ToShortDateString());
+     Console.WriteLine(now.ToString("F"));
+
+        }
+    }
+}
