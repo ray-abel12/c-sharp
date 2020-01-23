ee42e346ea63f26e90191b2da4a8ab40319aeb3f
ray-abel12 <rayabel12@outlook.com>
2020-01-23 16:26:02 +0100

    commit

---
+++ string.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace String
+{
+   public class String
+    {
+        static void Main(string[] args)
+        {
+            var fullName = "abel ray ";
+            Console.WriteLine($"trim {fullName.Trim()}");
+            Console.WriteLine($"trim and toupper {fullName.Trim().ToUpper()}");
+
+            //split it back
+            var name = fullName.Split(' ');
+            Console.WriteLine("firstName " + name[0]);
+            Console.WriteLine("lastName " + name[1]);
+         var replace = fullName.Replace("ray", "obeya");
+         Console.WriteLine(replace);
+        }
+    }
+}
