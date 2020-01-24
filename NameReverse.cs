4476ba70f0b128ef7e792825f615def5b18f6867
ray-abel12 <rayabel12@outlook.com>
2020-01-24 12:20:17 +0100

    commit

---
+++ NameReverse.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace NameReverse
+{
+    public class NameReverse
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter your name");
+            var name = Console.ReadLine();
+            
+           var array = new char[name.Length -1];
+           
+
+           for(var i =array.Length; i>=0; i--)
+            {
+                
+                var nameLength =Convert.ToString(name[i]) ;
+
+                var reverse =new string(nameLength);
+                Console.Write(nameLength);
+            }
+        }
+    }
+}
