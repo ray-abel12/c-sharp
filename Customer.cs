f59cce7936171d8d24d65e5f568548cf79ed544d
ray-abel12 <rayabel12@outlook.com>
2020-03-13 04:35:27 +0100

    commit

--- Customer.cs
+++ Customer.cs
@@ -1,19 +1,8 @@
-﻿using System;
-
-namespace ConstructorPlay
+﻿namespace ConstructorPlay
 {
     public class Customer
     {
         public int Id;
         public string Name;
-        public Customer(string play)
-        {
-            this.Name = play;
-        }
-        static void Main(string[] args)
-        {
-            var play = new Customer("abel");
-            Console.WriteLine(play);
-        }
     }
 }
