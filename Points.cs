35bb796d83c75a59a28a00989c1c6355cb283b07
ray-abel12 <rayabel12@outlook.com>
2020-03-13 15:37:59 +0100

    update

---
+++ Points.cs
@@ -0,0 +1,20 @@
+namespace Methods
+{
+    public class Points
+    {
+        public int X;
+        public int Y;
+
+        public Points(int x,int y)
+        {
+            this.X = y;
+            this.Y = y;
+        }
+
+        public void Move(int x, int y)
+        {
+            this.X = y;
+            this.Y = y;
+        }
+    }
+}
