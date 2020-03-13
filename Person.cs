1e0853d3afb8741cb37f03bf4dd1d7b6b916e5aa
ray-abel12 <rayabel12@outlook.com>
2020-03-13 03:45:56 +0100

    commit

---
+++ Person.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace FirstMethod
+{
+   public class Person
+   {
+       public String Name ;
+
+       public  void Introduce()
+       {
+           Console.WriteLine("Hello my name is "+ Name);
+       }
+
+       public static Person Parse(String str)
+       {
+          var program = new Person();
+          program.Name = str;
+          return program;
+       }
+       static void Main(string[] args)
+       {
+           var program = new Person();
+           var p = Person.Parse("john");
+           Console.WriteLine(p);
+           
+       }
+    }
+}
