2a0fc624148290a7808835ee0b08f42f990691f8
ray-abel12 <rayabel12@outlook.com>
2020-03-14 21:02:47 +0100

    update

---
+++ Text.cs
@@ -0,0 +1,13 @@
+namespace Inheritance
+{
+    public class Text : PresentationObjec
+    {
+        public int FrontSize { get; set; }
+        public string FrontName { get; set; }
+
+        public void AddHyperLink(string url)
+        {
+            
+        }
+    }
+}
