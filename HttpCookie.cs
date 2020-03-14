5a5d5c3cf605b132ac6aac1e2f998cb1df461510
ray-abel12 <rayabel12@outlook.com>
2020-03-14 17:40:29 +0100

    update

--- HttpCookie.cs
+++ HttpCookie.cs
@@ -1,7 +1,10 @@
+using System.Collections.Generic;
+
 namespace Indexer
 {
     public class HttpCookie
     {
-        
+        //key is string and value is string
+        private Dictionary<string, string> _dictionary;
     }
 }
