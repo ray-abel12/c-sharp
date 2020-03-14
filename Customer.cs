191b69938a5aa3256517a2916d89810ac4540d2d
ray-abel12 <rayabel12@outlook.com>
2020-03-14 15:01:05 +0100

    update

--- Customer.cs
+++ Customer.cs
@@ -1,29 +1,27 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 
-namespace ConstructorPlay
+namespace Fields
 {
     public class Customer
     {
         public int Id;
         public string Name;
-        public List<Order>Order;
-
-        public Customer()
-        {
-           Order = new List<Order>();
-        }
+        public List<Order> Orders = new List<Order>();
 
         public Customer(int id)
-        //loads the no-default constructor first 
-        :this()
         {
             this.Id = id;
         }
 
         public Customer(int id, string name)
-        :this(id)
+            : this(id)
         {
             this.Name = name;
+        }
+
+        public class Promote()
+        {
+            //......
         }
     }
 }
