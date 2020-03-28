cdcd699d03f2e6b3cac193f76fd0aa3eec21fd2a
ray-abel12 <rayabel12@outlook.com>
2020-03-28 14:31:04 +0100

    update

--- stringDemo.cs
+++ stringDemo.cs
@@ -6,7 +6,8 @@ namespace StringDemo
     {
         static void Main(string[] args)
         { 
-            var name = new string("abel");
+            /*var name = new string("abel");
+            var name2 = new string("abel");
             var num = "12345";
             
             var result = int.Parse(num);
@@ -15,9 +16,16 @@ namespace StringDemo
             Console.WriteLine(result2);
             Console.WriteLine(result);
            var newName = name.Replace('a', 'z');
+         var result3 =  string.Compare(name, name2);
+         Console.WriteLine(result3);
             Console.WriteLine(newName);
-            
-
+            */
+            var fullName = "abel ray";
+            var index = fullName.IndexOf(' ');
+            var name = fullName.Substring(0, index);
+            var lastName = fullName.Substring(index + 1);
+            Console.WriteLine(name);
+            Console.WriteLine(lastName);
         }
     }
 }
