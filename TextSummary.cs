ca1e507d9e61229b9ef6acb8294ab6275cff374c
ray-abel12 <rayabel12@outlook.com>
2020-03-28 20:59:29 +0100

    update

---
+++ TextSummary.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TextSummary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int maxLength = 20;
+            var sentence = "i just wanna type a very long text here";
+            
+            if(sentence.Length < 20 )
+                Console.WriteLine(sentence);
+            else
+            {
+                var words = sentence.Split(' ');
+                var totalCharacters = 0;
+                var summaryWords = new List<string>();
+
+                foreach (var word in words)
+                {
+                   summaryWords.Add(word); 
+                   totalCharacters += words.Length + 1;
+
+                   //   if (totalCharacters > maxLength)
+                   //    break;
+                }
+
+                var summary = string.Join("  ", summaryWords) + "...";
+                Console.WriteLine(summary);
+
+            }
+            
+        }
+    }
+}
