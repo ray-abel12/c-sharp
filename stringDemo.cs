fecbe03f3695f780c9ff8bf35750e5fba040c8a6
rayabel12 <rayabel12@outlook.com>
2020-03-28 10:36:08 +0100

    update

---
+++ stringDemo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace StringDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            var name = new string("abel");
+            var num = "12345";
+            
+            var result = int.Parse(num);
+            var result2 = Convert.ToInt32(result);
+            
+            Console.WriteLine(result2);
+            Console.WriteLine(result);
+           var newName = name.Replace('a', 'z');
+            Console.WriteLine(newName);
+            
+
+        }
+    }
+}
