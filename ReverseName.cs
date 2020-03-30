6796c470b21aa56a02284a47b3177ce5d77465db
ray-abel12 <rayabel12@outlook.com>
2020-03-30 10:30:10 +0100

    update

---
+++ ReverseName.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace reverseName
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+
+            
+            var name = Console.ReadLine();
+            var reversed = ReverseName(name);
+            Console.WriteLine("reverse name is " + reversed);
+            
+        }
+
+        public static string ReverseName(string nameInput)
+            {
+
+                var names = new char[nameInput.Length];
+
+                for (int i = names.Length; i > 0; i--)
+                {
+
+                    names[names.Length - i] = nameInput[i - 1];
+                }
+                
+                return new string(names);
+            }
+        }
+}
