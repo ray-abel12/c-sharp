94f586378eabbf7bf8aee3eac64a3ef7614ffc63
ray-abel12 <rayabel12@outlook.com>
2020-04-16 12:56:04 +0100

    update

---
+++ StringDublicate.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace StringDublicate
+{
+    class StringDublicate
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("enter numbers seperated by -");
+                var userInput = Console.ReadLine();
+                var numbers = userInput.Split('-').Select(text => Convert.ToInt32(text));
+                if (string.IsNullOrWhiteSpace(userInput)) break;
+            }
+        }
+    }
+}
