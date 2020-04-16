603d76c5cd5ee3e8f654af159af434009c92913f
ray-abel12 <rayabel12@outlook.com>
2020-04-16 11:28:11 +0100

    initial commit

--- Hyphen.cs
+++ Hyphen.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
 
 namespace Hypen_Numbers
 {
@@ -6,7 +9,23 @@ namespace Hypen_Numbers
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("");
+            Console.WriteLine("Enter number seperated by hypen");
+            var userInput = Console.ReadLine();
+            var numbers = userInput.Split('-').Select(text => Convert.ToInt32(text));
+            var numberList = new List<int>();
+            
+            foreach (var index in numbers)
+            {
+               numberList.Add(index);
+            }
+
+            var temp = numberList[0] - numberList[1];
+            
+            if((temp == 1) || (temp == -1)
+            {
+                Console.WriteLine("");
+            }
+
         }
     }
 }
