b29d1989e40fd96fd797d576956532574a6a82f8
ray-abel12 <rayabel12@outlook.com>
2020-04-18 19:59:00 +0100

    initial commit

--- PascalCase.cs
+++ PascalCase.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace PascalCase
 {
@@ -6,7 +7,25 @@ namespace PascalCase
     {
         static void Main(string[] args)
         {
-            
+            Console.Write("Enter a few words: ");
+            var input = Console.ReadLine();
+
+            if (String.IsNullOrWhiteSpace(input))
+            {
+                Console.WriteLine("Error");
+                return;
+            }
+
+            var variableName = "";
+            foreach (var word in input.Split(' '))
+            {
+                var wordWithPascalCase = char.ToUpper(word[0]) + word.ToLower().Substring(1);
+                Console.WriteLine(word[0]);
+                variableName += wordWithPascalCase;
+            }
+
+            Console.WriteLine(variableName);
+        }
+
         }
     }
-}
