8d0925b94a53477c7b6654186a2b2de2002293ad
ray-abel12 <rayabel12@outlook.com>
2020-04-18 14:18:03 +0100

    initial commit

---
+++ ValidTime.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace ValidDateTime
+{
+    class ValidTime
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter a valid time between 00:00 to 23:59");
+            var input = Console.ReadLine();
+
+            if (string.IsNullOrWhiteSpace(input))
+                return;
+
+            var time = Convert.ToDouble(input);
+            try
+            {
+            if((time >00.00) &&(time<23.59))
+            {
+                Console.WriteLine("valid time");
+            }
+           
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("invalid time");
+                throw;
+            }
+
+        }
+    }
+}
