6d118f9f3a7e47a3e7285998b3d97974c259a0d3
ray-abel12 <rayabel12@outlook.com>
2020-04-18 13:24:21 +0100

    initial commit

--- StringDublicate.cs
+++ StringDublicate.cs
@@ -9,7 +9,35 @@ namespace StringDublicate
     {
         static void Main(string[] args)
         {
-            
+            Console.WriteLine("enter values i.e(1-2-3-4)");
+            var input = Console.ReadLine();
+
+            if (string.IsNullOrWhiteSpace(input))
+                return;
+
+            var number = new List<int>();
+            foreach (var numbers in input.Split('-'))
+            {
+                number.Add(Convert.ToInt32(numbers));
+            }
+
+            var unique = new List<int>();
+            var isDuplicate = false;
+            foreach (var numbers in number)
+            {
+                if (!unique.Contains(numbers))
+                {
+                    unique.Add(numbers);
+                }
+                else
+                {
+                    isDuplicate = true;
+                    break;
+                }
+            }
+
+            var message = isDuplicate ? "Yes There is Duplicate" : "No Duplicate found";
+            Console.WriteLine(message);
         }
-        }
+    }
 }
