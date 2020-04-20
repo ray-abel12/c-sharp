7c227174c9d47b72ee830b3cc18d03ee0c4c6b29
ray-abel12 <rayabel12@outlook.com>
2020-04-20 10:34:19 +0100

    commit

---
+++ Reverse.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace reverseName
+{
+    class Reverse
+    {
+        static void Main(string[] args)
+
+        {
+
+            
+            var name = Console.ReadLine();
+            var reversed = ReverseName(name);
+            Console.WriteLine("reverse name is " + reversed);
+            
+        }
+
+        public static string ReverseName(string name)
+            {
+
+                var names = new char[name.Length];
+
+                for (int i = names.Length; i > 0; i--)
+                {
+
+                    names[names.Length - i] = name[i - 1];
+                }
+                
+                return new string(names);
+            }
+        }
+}
