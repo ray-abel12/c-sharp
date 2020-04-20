174dad051c51ab1ca6a16625eaccf37df6f93e7a
ray-abel12 <rayabel12@outlook.com>
2020-04-20 15:09:48 +0100

    commit

---
+++ FileAndFileInfo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+
+namespace FileAndFileInfo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = @"C:\Users\RAY-ABEL\folder\read.txt";
+          //  File.Copy(@"C:\Users\RAY-ABEL\folder\The_Hustle.mp4",@"C:\Users\RAY-ABEL\destination\test.txt,true");
+      // var text = File.ReadAllText(path);
+       //Console.WriteLine(text);
+       
+       var fileInfo = new FileInfo(path);
+       //fileInfo.CopyTo(..)
+       var check = fileInfo.Exists;
+       Console.WriteLine(check);
+        }
+    }
+}
