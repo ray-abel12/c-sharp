be0cc3ec93d3fe642f14906f03020788bc55424a
ray-abel12 <rayabel12@outlook.com>
2020-04-20 15:52:45 +0100

    commit

---
+++ Path.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO;
+
+namespace Path
+{
+    class Path
+    {
+        static void Main(string[] args)
+        {
+            var path = @"C:\code\c\ReverseName.txt";
+            Console.WriteLine(System.IO.Path.GetExtension(path));
+            
+        }
+    }
+}
